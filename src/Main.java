bedfe9c1e8c5456476159ad0697afc86860f9048
studentwsb <gralik.kamil@wp.pl>
2021-09-26 12:58:02 +0200

    cw1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.print("A więc! ");
+        System.out.println("Zaczynamy przygodę z Java ");
 
+        System.out.print("Będzie to ");
+        System.out.print("trudna przeprawa");
+        System.out.println(" ale dam radę!");
+
+        System.out.printf("Inaczej nie nazywam się %s, student WSB!\n", "Kamil");
     }
 }
