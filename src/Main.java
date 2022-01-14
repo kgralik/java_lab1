d1bbe6b8c47323e22c0a369eea2a6db9332751fb
k.gralik <gralik.kamil@wp.pl>
2022-01-14 14:44:09 +0100

    cw3

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,17 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Podaj wiek: ");
+        String wiek = scan.next();
+        System.out.print("Podaj nr indeksu: ");
+        String indeks = scan.next();
+        System.out.printf("Imię: " + imie + "%n" + "Nazwisko: " + nazwisko + "%n" + "Wiek: " + wiek + "%n" + "Numer indeksu: " + indeks + "%n");
+
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
@@ -19,7 +30,12 @@ public class Main {
         System.out.println("Cwiczenie 3");
         System.out.println("zad1 a");
         Scanner scan = new Scanner(System.in);
-
+        Scanner kalkulator = new Scanner(System.in);
+        System.out.print("Podaj pierwszą liczbę:");
+        Double a = kalkulator.nextDouble();
+        System.out.print("Podaj drugą liczbę");
+        Double b = kalkulator.nextDouble();
+        System.out.println("a = " + a + "%n" + "b = " + b);
 
     }
 }
