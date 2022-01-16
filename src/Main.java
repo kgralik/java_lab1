4bd85c0a5c29e440b7d10186580138c3f7d343a6
k.gralik <gralik.kamil@wp.pl>
2022-01-16 10:21:42 +0100

    cw4

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,21 @@ public class Main {
 
         /* ćw.4 */
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        /* zad.1 Proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+
+        System.out.println("(a!=b)" + (a != b));
+        System.out.println("(a&&b)" + (a && b));
+        System.out.println("(b||c)" + (b || c));
 
 
+        int x = 8;
+        int y = 12;
+        int z = 19;
+        System.out.println("(x>z)" + (x > z));
+        System.out.println("((a&&b)||(x>x))" + ((a && b) || (x > x)));
     }
 }
